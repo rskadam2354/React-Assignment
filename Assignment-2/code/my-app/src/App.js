f7772613ebfea8b0a5988e4518b2c238dd287acd
Shinorikishi <kadamrohitsandeep@gmail.com>
2022-02-05 14:34:24 +0530

    Assignment 2 react submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-
 
 function App(props) {
  const {employee}=props;
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+  <div>
+    <img src={employee.profileImg} width="100px" height="100px" alt="This is a profile pic"/>
+  </div>
+  <div>
+    <p><h1>{employee.name}</h1></p>
+  </div>
+  <div>
+    <p><section>Location</section>{employee.location}</p>
+  </div>
+  <div>
+    <p><section>Blood Group</section>{employee.bloodGroup}</p>
+  </div>
+  <div>
+    <p><section>Age</section>{employee.age}</p>
+  </div>
 </div>
 )
 }
